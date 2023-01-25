5a24dcd67634ff89d81cd9abe2e632c4bd1feb2d
cannddy <itspraveen2004@gmail.com>
2023-01-25 23:01:34 +0530

    update ignored file

--- resources/How to install and use the python face recognition and detection library in Ubuntu 16.04 Our Code World_files/count-data.js
+++
@@ -1,5 +0,0 @@
-var DISQUSWIDGETS;
-
-if (typeof DISQUSWIDGETS != 'undefined') {
-    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"\/articles\/read\/841\/how-to-install-and-use-the-python-face-recognition-and-detection-library-in-ubuntu-16-04","comments":0}]});
-}
